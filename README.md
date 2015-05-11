32acb69efa3e5588b30cac8e7092b3b2fc14b6dc
Josh Zaslow <josh.zaslow@gmail.com>
2015-05-10 23:33:15 -0400

    Initial commit. Lots to do, but this works in its own rough, non-flexible way

---
+++ README.md
@@ -0,0 +1,9 @@
+# YammTeX (Working title)
+
+* article._tex -- goes in ~/.yammTex
+* yammtex.py -- symbolic linked to a location in path
+* YammTex.sublime-build -- goes in Sublime\ Packages/User
+
+Inspired by the writing system of Markdown + YAML used in Jekyll, I wanted a system that would allow me to do something similar for TeX files. More often than not, I reuse my LaTeX styles, and import a file of content. This is fairly limited to the possibilities for templating I can do with a more Jekyll-like system, so off I go!
+
+Current version is written in python. It needs tests and error handling and flexible code. But its far enough along that it actually works... so that's pretty awesome! I can also currently build these files from Sublime.
---
+++ yammtex.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+import sys, os
+from os.path import join, exists, getmtime
+from os.path import expanduser
+from subprocess import call
+import re
+import yaml
+
+import argparse
+
+from jinja2 import Environment
+from jinja2 import BaseLoader, TemplateNotFound
+
+
+def escape_tex(value):
+    """ This function allows us to redefine the jinja environment \
+     to handle LaTeX environments """
+    # This code, and the code that call this is courtesy of Clemens Kaposi
+    # http://flask.pocoo.org/snippets/55/
+
+    LATEX_SUBS = (
+        (re.compile(r'\\'), r'\\textbackslash'),
+        (re.compile(r'([{}_#%&$])'), r'\\\1'),
+        (re.compile(r'~'), r'\~{}'),
+        (re.compile(r'\^'), r'\^{}'),
+        (re.compile(r'"'), r"''"),
+        (re.compile(r'\.\.\.+'), r'\\ldots'),
+    )
+
+    newval = value
+    for pattern, replacement in LATEX_SUBS:
+        newval = pattern.sub(replacement, newval)
+    return newval
+
+
+class TeXLoader(BaseLoader):
+    """ This environment loader allows us to readily customize \
+    the Jinja2 BaseLoader class for our purposes """
+
+    def __init__(self, path):
+        self.path = path
+
+    def get_source(self, environment, template):
+        path = join(self.path, template)
+        if not exists(path):
+            raise TemplateNotFound(template)
+        mtime = getmtime(path)
+        with file(path) as f:
+            source = f.read().decode('utf-8')
+        return source, path, lambda: mtime == getmtime(path)
+
+
+class YammTex():
+
+    def __init__(self, target_file):
+        self.target_file = target_file
+        self.output_file = target_file.replace('.yd', '.tex')
+        self.variables = self.load_variables()
+        self.home = expanduser("~")
+        self.template_dir = self.home + '/.yammTeX'
+        self.default_template = self.template_dir + '/default._tex'
+
+
+    def load_variables(self, division_string="---\n"):
+        stream = open(self.target_file, 'r')
+
+        start_yaml = False
+        end_yaml = False
+        variables = ""
+        content = ""
+
+        for line in stream:
+            if str(line) == division_string:
+
+                if start_yaml:
+                    end_yaml = True
+                else:
+                    start_yaml = True
+            else:
+                if start_yaml and not end_yaml:
+                    variables += line
+                else:
+                    content += "  " + line + "\n"
+
+        variables += "content: > \n" + content
+
+        return yaml.load(variables)
+
+    def make_file(self):
+        texenv = Environment(loader=TeXLoader(self.home))
+        texenv.block_start_string = '((*'
+        texenv.block_end_string = '*))'
+        texenv.variable_start_string = '((('
+        texenv.variable_end_string = ')))'
+        texenv.comment_start_string = '((='
+        texenv.comment_end_string = '=))'
+        texenv.filters['escape_tex'] = escape_tex
+
+        if self.variables["template"]:
+            template_file = self.template_dir + '/' + self.variables["template"] + '._tex'
+        else:
+            template_file = default
+
+        #6. Run through the template
+        template = texenv.get_template(template_file)
+        # template = Template('Hello {{ name }}\n\n{{ content }}!')
+
+        f = open(self.output_file, "w")  #opens file with name of "test.txt"
+        f.write(template.render(self.variables))
+        f.close()
+
+        print "LaTeX file written"
+        print "---"
+
+    def make_pdf(self):
+        print "Generating PDF"
+        print "---"
+        call(["pdflatex", "", self.output_file])
+
+    def render(self):
+        self.make_file()
+        self.make_pdf()
+
+
+def main(target_file, output_file="", watch=False):
+    new_file = YammTex(target_file)
+
+    if watch:
+        print "---"
+        print "Performing initial rendering"
+        print "---"
+        last_time = False
+        while True:
+            if last_time != os.stat(target_file).st_mtime:
+                last_time = os.stat(target_file).st_mtime
+                new_file.render()
+                print "---"
+                print "Watching {}".format(target_file)
+                print "---"
+    else:
+        new_file.render()
+
+
+# This should be properly updated to use argparse
+if __name__ == "__main__":
+
+    if len(sys.argv) >= 2 and sys.argv[1] == "watch":
+        print "watch mode!"
+        if len(sys.argv) >= 3:
+            target_file = sys.argv[2]
+        else:
+            raise Exception
+
+        if len(sys.argv) >= 4:
+            output_file = sys.argv[3]
+
+        main(target_file, watch=True)
+    else:
+        if len(sys.argv) >= 2:
+            target_file = sys.argv[1]
+        else:
+            raise Exception
+
+        if len(sys.argv) >= 3:
+            output_file = sys.argv[2]
+
+        main(target_file)
