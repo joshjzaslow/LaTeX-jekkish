b77f8f20bb6aee337eca8c8ba9eeb422f9bfe2db
Josh Zaslow <josh.zaslow@gmail.com>
2015-05-15 20:24:35 -0400

    More PyPI updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'jekkish',
     packages = ['jekkish'], # this must be the same as the name above
-    version = '0.1',
+    version = '0.1.1',
     description = 'A templated LaTeX renderer inspired by Jekyll',
     author = 'Josh Zaslow',
     author_email = 'josh.zaslow@gmail.com',
