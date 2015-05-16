917e903fb014f46cb139fce1cdbcb0754d3c59be
Josh Zaslow <josh.zaslow@gmail.com>
2015-05-15 20:19:08 -0400

    Fixed URLs in setup.py.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     description = 'A templated LaTeX renderer inspired by Jekyll',
     author = 'Josh Zaslow',
     author_email = 'josh.zaslow@gmail.com',
-    url = 'https://github.com/joshjzaslow/jekkish', # use the URL to the github repo
-    download_url = 'https://github.com/joshjzaslow/jekkish/tarball/0.1', # I'll explain this in a second
+    url = 'https://github.com/joshjzaslow/latex-jekkish', # use the URL to the github repo
+    download_url = 'https://github.com/joshjzaslow/latex-jekkish/tarball/0.1', # I'll explain this in a second
     keywords = ['LaTeX'], # arbitrary keywords
     classifiers = [],
 )
