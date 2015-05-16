5c14d8f3069480233339f9fd1d8e8b019fbd0818
Josh Zaslow <josh.zaslow@gmail.com>
2015-05-15 22:00:02 -0400

    Updating setup for 0.1.4. Someday these numbers will mean something.

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
 setup(
     name = 'jekkish',
-    packages = ['jekkish'], # this must be the same as the name above
-    version = '0.1.1',
+    packages = ['jekkish'],
+    entry_points = {
+      "console_scripts": ['jekkish = jekkish.jekkish:main']
+    },
+    version = '0.1.4',
     description = 'A templated LaTeX renderer inspired by Jekyll',
     author = 'Josh Zaslow',
     author_email = 'josh.zaslow@gmail.com',
-    url = 'https://github.com/joshjzaslow/latex-jekkish', # use the URL to the github repo
-    download_url = 'https://github.com/joshjzaslow/latex-jekkish/tarball/0.1.1', # I'll explain this in a second
-    keywords = ['LaTeX'], # arbitrary keywords
+    url = 'https://github.com/joshjzaslow/latex-jekkish',
+    download_url = 'https://github.com/joshjzaslow/latex-jekkish/tarball/0.1.4',
+    keywords = ['LaTeX'],
     classifiers = [],
+    install_requires = ['PyYAML', 'Jinja2'],
 )
