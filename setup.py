54132c3b4440b8336e076dffafa1ad79fbc0d3ad
Josh Zaslow <josh.zaslow@gmail.com>
2015-05-15 20:25:18 -0400

    PyPI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author = 'Josh Zaslow',
     author_email = 'josh.zaslow@gmail.com',
     url = 'https://github.com/joshjzaslow/latex-jekkish', # use the URL to the github repo
-    download_url = 'https://github.com/joshjzaslow/latex-jekkish/tarball/0.1', # I'll explain this in a second
+    download_url = 'https://github.com/joshjzaslow/latex-jekkish/tarball/0.1.1', # I'll explain this in a second
     keywords = ['LaTeX'], # arbitrary keywords
     classifiers = [],
 )
