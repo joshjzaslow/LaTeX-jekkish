e5606e18a59f84e7302d36e8c29822ac2f694394
Josh Zaslow <josh.zaslow@gmail.com>
2015-05-15 20:06:15 -0400

    Prepping code for PyPI submission

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'jekkish',
+    packages = ['jekkish'], # this must be the same as the name above
+    version = '0.1',
+    description = 'A templated LaTeX renderer inspired by Jekyll',
+    author = 'Josh Zaslow',
+    author_email = 'josh.zaslow@gmail.com',
+    url = 'https://github.com/joshjzaslow/jekkish', # use the URL to the github repo
+    download_url = 'https://github.com/joshjzaslow/jekkish/tarball/0.1', # I'll explain this in a second
+    keywords = ['LaTeX'], # arbitrary keywords
+    classifiers = [],
+)
